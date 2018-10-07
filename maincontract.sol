5e87dfa47de5a05ae6738fa2a3ad83bad2266f6d
freedgelink <43918406+freedgelink@users.noreply.github.com>
2018-10-07 07:46:22 -0700

    Update maincontract.sol

--- maincontract.sol
+++ maincontract.sol
@@ -12,6 +12,7 @@ contract payroll{
     uint Balance;
     uint AvailableBalance;
     uint ReservedBalance;
+    uint GasReserve
     
     uint256 EmployeeCount;
     uint256 ID = 1;
@@ -22,6 +23,7 @@ contract payroll{
         require (msg.sender == owner);
         _;
     }
+   
 
 struct Employees{
         string name;
@@ -64,11 +66,8 @@ struct Employees{
         _Balance[Name] = 0;
         _Lock[Name] = true;
         EmployeeCount--;
+        }
     }
-}
-    
-
-    /* Mapping */
     
     mapping (uint256 => string) _Name;
     mapping (uint256 => uint256) _IDnumber;
@@ -77,17 +76,105 @@ struct Employees{
     mapping (uint256 => bool) _Lock;
     
     mapping (address => uint256) IDbyAddress;
-    
-     /* Modifiers */
 
     modifier Owned(){
         require (msg.sender == owner);
         _;
     }
     
-    /* Event */
     event broadcast(string);
+
+    }
+    
+    function redistribute() internal {
+        Balance = address(this).balance;
+        if(Balance > ReservedBalance + GasReserve){
+        AvailableBalance = Balance - (ReservedBalance + GasReserve);
+        }
+    } 
+    
+    function Distribute() internal{
+        require(EmployeeCount > 0);
+        redistribute();
+        require(AvailableBalance > 0);
+        uint Payout = AvailableBalance / EmployeeCount;
+        for(uint i=1; i <= ID -1; i++) {
+            if(_IDnumber[i] >= 1 ){
+            _Balance[i] += Payout ;
+        }
+       }
+        ReservedBalance += AvailableBalance;
+        AvailableBalance = 0;
+    }
+    
+    /* Test Functions */
+      
+    function WhatsTheDay() public view returns(uint) {
+        return block.number;
+    }
+
+    function TestDistribute() public Owned{
+        Distribute();
+    }
+    
+    function TestRedistribute() public Owned{
+        redistribute();
+    }
+    
+    function ContractBalanceWei() public view returns (uint){
+        return address(this).balance;
+    }
+    
+    function TestAvailable_balance() public view returns (uint){
+        return(AvailableBalance);
+    }
 
 }
 
-  
+function onboard(string Name, address Address) public Owned {
+        _Name[ID] = Name;
+        _IDnumber[ID] = ID;
+        _Address[ID] = Address;
+        _Balance[ID] = 0;
+        _Lock[ID] = false;
+        IDbyAddress[Address] = ID;
+        EmployeeCount++;
+        ID++;
+        
+        GasReserve = EmployeeCount * 1e9 wei;
+
+        emit broadcast(Name);
+        
+    }
+    
+    function getbyID(uint256 EmployeeID) public view Owned returns(string, address, uint256, bool){
+        return(_Name[EmployeeID], _Address[EmployeeID], _Balance[EmployeeID], _Lock[EmployeeID]);
+    }
+    
+    function EmployeeLock(uint256 EmployeeID, bool Toggle) public Owned {
+        _Lock[EmployeeID] = Toggle;
+    }
+    
+    function changeAddress(uint256 EmployeeID, address Address) public Owned{
+        require(_Lock[EmployeeID] == true);
+        _Address[EmployeeID] = Address;
+    }
+    
+    function DeleteUser(uint256 EmployeeID) public Owned{
+        require(EmployeeCount > 0);
+        require(_Lock[EmployeeID] == true);
+        AvailableBalance = AvailableBalance + _Balance[EmployeeID];
+        ReservedBalance = ReservedBalance - _Balance[EmployeeID];
+        IDbyAddress[_Address[EmployeeID]] = 0;
+        _Name[EmployeeID] = '';
+        _IDnumber[EmployeeID] = 0;
+        _Address[EmployeeID] = 0;
+        _Balance[EmployeeID] = 0;
+        _Lock[EmployeeID] = true;
+        EmployeeCount--;
+        
+        GasReserve = EmployeeCount * 1e9 wei;
+    }
+}
+
+
